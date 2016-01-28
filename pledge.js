6ae5de30131aa09b68d0f72c14f54d72cb46c720
Michael Bushoy <mbushoy@gmail.com>
2016-01-28 12:07:26 -0500

    ch2

--- pledge.js
+++ pledge.js
@@ -4,8 +4,61 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
+function $Promise() {
+   this.state='pending';
+   this.handlerGroups=[];
+   this.callHandlers=function(data) {
+      console.log(this.handlerGroups[0])
+      if (this.state=='resolved' && typeof this.handlerGroups[0].successCb=='function' && this.handlerGroups.length>0)
+         this.handlerGroups.shift().successCb(data);
+      else if (this.state=='rejected' && typeof this.handlerGroups[0].errorCb=='function')
+         this.handlerGroups.shift().errorCb(data);
+      }     
+   this.then=function(success,fail) {
+      // if (typeof success == 'function' && typeof fail == 'function')
+      //    this.handlerGroups.push({successCb: success, errorCb: fail});
+      if (typeof success != 'function' && typeof fail != 'function')
+         this.handlerGroups.push({successCb: null, errorCb: null});
+      else
+         this.handlerGroups.push({successCb: success, errorCb: null});
+      // else if (typeof fail == 'function')
+      //    this.handlerGroups.push({successCb: null, errorCb: fail});
 
+      if (this.state='resolved')
+         this.callHandlers(this.value);
+   };
 
+}
+
+//$Promise.prototype.
+
+// $Promise.prototype.
+
+function Deferral() {
+   this.$promise=new $Promise();
+}
+
+Deferral.prototype.resolve = function(data) {
+      if (this.$promise.state=='pending')  {
+      this.$promise.state='resolved';
+      this.$promise.value=data;
+      if (this.$promise.handlerGroups.length>0) {
+      for (var x=0;x<this.$promise.handlerGroups.length;x++)
+         this.$promise.callHandlers(data);
+      }
+   }
+   }
+
+Deferral.prototype.reject = function(reason) {
+   if (this.$promise.state=='pending')  {
+   this.$promise.state='rejected';
+   this.$promise.value=reason;
+   }
+}
+
+function defer() {
+   return new Deferral();
+};
 
 
 
